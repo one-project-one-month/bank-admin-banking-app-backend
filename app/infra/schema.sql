f77381ee58ed0a11f1f241eb8a37137e4c4e1ce8
dev-cynic <htetaunglynn@my.uopeople.edu>
2025-10-02 16:37:32 +0700

    insert acount types into account_type table

--- app/infra/schema.sql
+++ app/infra/schema.sql
@@ -199,3 +199,9 @@ INSERT INTO "nrc_code_value" ("code_id", "value") VALUES
 INSERT INTO "nrc_code_value" ("code_id", "value") VALUES
 (16, '1'),(16, '2'),(16, '3'),(16, '4'),(16, '5'),(16, '6'),(16, '7'),
 (16, '8'),(16, '9'),(16, '10'),(16, '11'),(16, '12'),(16, '13'),(16, '14');
+
+
+INSERT INTO "account_type" (name) VALUES
+                                      ('Savings'),
+                                      ('Special'),
+                                      ('Origanization');
