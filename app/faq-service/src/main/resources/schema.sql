68531462b34a58fc5d12ac23804fab988d5ec731
Show Wai Yan <showwaiyan555@gmail.com>
2025-10-05 11:07:58 +0800

    Add faq_category table and set FK in faq table

--- app/faq-service/src/main/resources/schema.sql
+++ app/faq-service/src/main/resources/schema.sql
@@ -1,7 +1,15 @@
+CREATE TABLE IF NOT EXISTS faq_category(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL default now(),
+    updated_at TIMESTAMP
+);
+
 CREATE TABLE IF NOT EXISTS faq(
     id SERIAL PRIMARY KEY,
     question TEXT NOT NULL,
     answer TEXT NOT NULL,
+    faq_category_id INT REFERENCES FAQ_Category(id) ON DELETE SET NULL,
     created_at TIMESTAMP NOT NULL default now(),
     updated_at TIMESTAMP
 );
